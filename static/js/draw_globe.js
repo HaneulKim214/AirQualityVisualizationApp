b96520aad09a8c7d5fae9f2e9205305be320ceda
haneul kim <haneulkim214@gmail.com>
2019-06-08 08:52:04 -0400

    3d-globe

--- static/js/draw_globe.js
+++ static/js/draw_globe.js
@@ -1,8 +1,69 @@
-var planet = planetaryjs.planet();
+var globe = planetaryjs.planet();
 var canvas = document.getElementById('globe');
-// You can remove this statement if `world-110m.json`
-// is in the same path as the HTML page:
 
-// Make the planet fit well in its canvas
-planet.projection.scale(250).translate([250, 250]);
-planet.draw(canvas); 
+function autorotate(degPerSec) {
+    // Planetary.js plugins are functions that take a `planet` instance
+    // as an argument...
+    return function(planet) {
+        var lastTick = null;
+        var paused = false;
+        planet.plugins.autorotate = {
+        pause:  function() { paused = true;},
+        resume: function() { paused = false;}
+        };
+        // ...and configure hooks into certain pieces of its lifecycle.
+        planet.onDraw(function() {
+        if (paused || !lastTick) {
+            lastTick = new Date();
+        } else {
+            var now = new Date();
+            var delta = now - lastTick;
+            // This plugin uses the built-in projection (provided by D3)
+            // to rotate the globe each time we draw it.
+            var rotation = planet.projection.rotate();
+            rotation[0] += degPerSec * delta / 1000;
+            if (rotation[0] >= 180) rotation[0] -= 360;
+            planet.projection.rotate(rotation);
+            lastTick = now;
+        }})
+    }};
+    
+    
+    // The `zoom` and `drag` plugins enable
+    // manipulating the globe with the mouse.
+    globe.loadPlugin(planetaryjs.plugins.zoom({
+    scaleExtent: [300, 500] // [min, max]
+    }));
+    
+    globe.loadPlugin(planetaryjs.plugins.drag({
+    // Dragging the globe should pause the
+    // automatic rotation until we release the mouse.
+    onDragStart: function() {
+        this.plugins.autorotate.pause();
+    },
+    onDragEnd: function() {
+        this.plugins.autorotate.resume();
+    }
+    }));
+    
+    // calling autorotate plugin built above.
+    globe.loadPlugin(autorotate(1));
+    globe.loadPlugin(planetaryjs.plugins.earth({
+    oceans:   { fill:   '#000080' },
+    land:     { fill:   '#339966' },
+    borders:  { stroke: '#008000' }
+    }));
+    
+    
+    
+    
+    
+    
+    
+    
+    // Make the planet fit well in its canvas
+    globe.projection
+        .scale(canvas.width/2)
+        .translate([canvas.width / 2, canvas.height/2]);
+    
+    globe.draw(canvas);
