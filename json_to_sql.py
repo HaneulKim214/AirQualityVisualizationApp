0d6c2dc3dea843ff97e31eaf292a13698bdfa8c8
haneul kim <haneulkim214@gmail.com>
2019-07-17 14:04:51 -0400

    chaning front-end

--- json_to_sql.py
+++ json_to_sql.py
@@ -2,31 +2,46 @@ from itertools import chain, repeat
 import json
 import os
 from pprint import pprint
+import pymysql
 import pandas as pd
 import requests
+import sqlalchemy
+
+# connect to db names 'aqi_db'
+engine = sqlalchemy.create_engine('mysql+pymysql://root:Gksmf12#@localhost:3306/aqi_db')
+
+# loading json file into data variable
+with open('static/db/countries_cities.json') as f:
+    data = json.load(f)
+aqi_list = []
 
 beijing_api = os.environ.get('AQI_api_key')
+
+# for each city, if aqi data exists append it to aqi_list
 def get_aqi(city):
     url = f'https://api.waqi.info/feed/{city}/?token={beijing_api}'
     response = requests.get(url).json()
-    # if station DNE return none otherwise return AQI information
-    if response['status'] == 'error':
-        return None
-    else:
-        #return AQI info for each city.
-        return response["data"]["aqi"]
-
-# loading json file into data variable
-with open('static/db/sample.json') as f:
-    data = json.load(f) 
+    if response['status'] == 'ok':
+        aqi_list.append(response["data"]["aqi"])
+    else: 
+        aqi_list.append(None)
+        pass
 
 chainer = chain.from_iterable
 countries_cities_df = pd.DataFrame.from_dict({'Countries':list(chainer(repeat(k, len(v)) for k,v in data.items())),
                             'Cities':list(chainer(data.values()))})
 
-# for each city, get AQI data and add it to new column called "AQI".
 for index, row in countries_cities_df.iterrows():
     city = countries_cities_df["Cities"].values[index]
-    countries_cities_df["AQI"] = get_aqi(city) # passing in city name to get_aqi function which returns AQI data.
+    get_aqi(city)
+    
+# adding AQI column into df.
+countries_cities_df["AQI"] = aqi_list
 
-countries_cities_df.head()
+# convert df -> SQL
+countries_cities_df.to_sql(
+    name="aqi_info",
+    con=engine,
+    index=False,
+    if_exists="append"
+)
--- reverseGeocoding.py
+++
@@ -1,8 +0,0 @@
-def reverseGeocode(city_name):
-    """given name of the city, use google API tograb their lat,lng
-    and store them into a list."""
-
-    lat_lng = []
-    # [[lat,lng], [lat,lng], ...]
-
-    
