c23e810006f6ba6eff7bf4eecca70811532cbb96
Haneulkim214 <haneulkim214@gmail.com>
2020-11-25 22:48:20 +0900

    changed some config

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 // API key for aqicn.org/api/ -> beijingapi
 var beijing_api = "53efafc18c686b9dcae32b983edb6db4f3ef23d8"
-var leaflet_api = "pk.eyJ1IjoiaGFuZXVsa2ltMjE0IiwiYSI6ImNqdnB0cjJtMjJkMmM0NW9pNmN0bjFyZjUifQ.v8Hz5-t66g5-AmrRQXDfDQ"; 
+var leaflet_api = "pk.eyJ1IjoiaGFuZXVsa2ltMjE0IiwiYSI6ImNraGJxNTUweTAwa2MydXRndHl6NWZpb20ifQ.QSVq0NcGPqqURH9l1aPoww"; 
